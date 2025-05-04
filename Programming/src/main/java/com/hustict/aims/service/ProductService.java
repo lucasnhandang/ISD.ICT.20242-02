34ec9fd03ac165c77a1bde6640ba9741b4dffc1f
BaoThu <hobaothuclone2@gmail.com>
2025-05-04 22:54:11 +0700

    Update ProductService.java

--- Programming/src/main/java/com/hustict/aims/service/ProductService.java
+++ Programming/src/main/java/com/hustict/aims/service/ProductService.java
@@ -4,6 +4,14 @@ import com.hustict.aims.exception.*;
 import com.hustict.aims.model.Product;
 
 public class ProductService {
+
+    public void restock(Product product, int quantity) {
+        if (quantity <= 0) {
+            throw new IllegalArgumentException("Restock quantity must be positive!");
+        }
+        product.setTotalQuantity(product.getTotalQuantity() + quantity);
+    }
+
     public boolean checkAvailability(Product product, int quantity) {
         if (product.getTotalQuantity() <= 0) {
             throw new OutOfStockException(product.getTitle() + " is out of stock.");
