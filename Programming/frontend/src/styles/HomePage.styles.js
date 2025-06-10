b0e841586f417b5851ec8461a14c70af81a66586
nhi nguyễn <nhi.nl225991@sis.hust.edu.vn>
2025-06-11 00:30:49 +0700

    update front end for homepage

---
+++ Programming/frontend/src/styles/HomePage.styles.js
@@ -0,0 +1,29 @@
+export const containerStyles = {
+  mt: 4,
+  mb: 4,
+  flexGrow: 1
+};
+
+export const loadingContainerStyles = {
+  display: 'flex',
+  justifyContent: 'center',
+  mt: 4
+};
+
+export const messageContainerStyles = {
+  textAlign: 'center',
+  mt: 4
+};
+
+export const paginationContainerStyles = {
+  display: 'flex',
+  justifyContent: 'center',
+  mt: 4,
+  mb: 4
+};
+
+export const rootStyles = {
+  minHeight: '100vh',
+  display: 'flex',
+  flexDirection: 'column'
+}; 
