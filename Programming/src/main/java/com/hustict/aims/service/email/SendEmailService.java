039a99acd3e5bbc80273a9526cfed3731a627e14
thowo <hobaothu202@gmail.com>
2025-06-21 17:22:10 +0700

    Update Place Order

--- Programming/src/main/java/com/hustict/aims/service/email/SendEmailService.java
+++ Programming/src/main/java/com/hustict/aims/service/email/SendEmailService.java
@@ -1,6 +1,5 @@
 package com.hustict.aims.service.email;
 
 public interface SendEmailService {
-
-    void send(String to, String subject, String body);
+    void sendEmail(Order order);
 }
---
+++ Programming/src/main/java/com/hustict/aims/service/email/SendEmailServiceImpl.java
@@ -0,0 +1,9 @@
+package com.hustict.aims.service.email;
+
+import org.springframework.mail.javamail.JavaMailSender;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.Context;
+
+public abstract class SendEmailServiceImpl implements SendEmailService {
+
+}
