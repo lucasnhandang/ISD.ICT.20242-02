0cd5c4dd3bf6cbea7157f122d51bf651077297d4
nhi nguyễn <nhi.nl225991@sis.hust.edu.vn>
2025-06-30 00:02:49 +0700

    fixx

--- Programming/frontend/src/App.jsx
+++ Programming/frontend/src/App.jsx
@@ -87,7 +87,7 @@ function App() {
           <Route path="/management/*" element={<ManagementPanel />} />
           <Route path="/order-review" element={<OrderReviewPage />} />
           <Route path="/rush-order" element={<RushOrderPage />} />
-          <Route path="/invoice/:id" element={<InvoicePage />} />
+          <Route path="/invoice" element={<InvoicePage />} />
           <Route path="/payment-result" element={<PaymentResultPage />} />
           <Route path="/vnpay-return" element={<VnPayReturnPage />} />
           <Route path="/api-test" element={<ApiTestComponent />} />
